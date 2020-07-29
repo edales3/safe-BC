9da30585c179cb44540bc35af1c4d5c8825ad264
edales3 <54680786+edales3@users.noreply.github.com>
2020-07-29 13:40:08 +0200

    Add files via upload

--- Simulator/elu.m
+++ Simulator/elu.m
@@ -1,15 +1,8 @@
-function g = sigmoid(z)
-%SIGMOID Compute sigmoid function
-%   g = SIGMOID(z) computes the sigmoid of z.
+function g = elu(z)
+%ELU Compute Exponential Linear Unit function
 
-% You need to return the following variables correctly 
 g = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
-
-%g = ones(size(z)) ./ (1 + exp(-z));
 for i =1:length(z)
     if z(i)<0
         g(i) = 1.0*(exp(z(i)) - 1);
